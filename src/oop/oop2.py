f13a0a8396870eeb507218f1b531b1568c1871df
ferror18 <ferror18@gmail.com>
2020-08-07 09:42:19 -0700

    MVP

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,9 +4,10 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
-
+    def drive(self):
+        return 'vroooom'
     # TODO
 
 
@@ -18,7 +19,11 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        self.num_wheels = 2
+    def drive(self):
+            return "BRAAAP!!"
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
